695793fba798677ff7782dd17e0fe43fb84d139b
MegaBebra <yakimenko_nikolay@list.ru>
2023-03-20 19:37:24 +0300

    Initial Commit

---
+++ EmojiReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by Николай Якименко on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
